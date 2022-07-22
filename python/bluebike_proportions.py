2d7ac07af81ba2ff63369ce392690d9c0ae73c46
Edward-HartleyICL <edward.hartley20@imperial.ac.uk>
2022-07-22 16:14:04 +0100

    bluebikes proportions etimated again
    -- final method was not using the bluebikes porportion file but have included changes anyway

--- python/bluebike_proportions.py
+++ python/bluebike_proportions.py
@@ -17,7 +17,7 @@ bluebikes_trips_augmented = get_data('./data/bluebikes/relevant_bluebikes_trips_
 # Checks reveal this is a poor way to predict replica distances from replica lat/lng
 all_trips['distance_km'] = all_trips.apply(lambda x: x['distance_miles'] * 1.6, axis=1)
 # all_trips['distance_straight'] = all_trips.apply(lambda x: get_distance(x['origin_lat'], x['origin_lng'], x['destination_lat'], x['destination_lng'])/1600, axis=1)
-# all_trips['distance_estimated'] = all_trips.apply(lambda x: get_OSRM_distance('driving', x['origin_lat'], x['origin_lng'], x['destination_lat'], x['destination_lng'])/1000, axis=1)
+all_trips['distance_estimated'] = all_trips.apply(lambda x: get_OSRM_distance('biking', x['origin_lat'], x['origin_lng'], x['destination_lat'], x['destination_lng'])/1000, axis=1)
 
 # estimate cycled distances for bluebikes trips using parameters from the all_trips dataframe
 bluebikes_trips_augmented['distance_estimated'] = bluebikes_trips_augmented.apply(lambda x: (get_OSRM_distance('biking', x['start station latitude'], x['start station longitude'], x['end station latitude'], x['end station longitude']) / 1000), axis=1)
@@ -30,7 +30,7 @@ cycle_total = 0
 bluebikes_total = 0
 
 for i in range(1, len(boundaries)):
-    all_trips_distance = all_trips[(all_trips['distance_km'] >= boundaries[i-1]) & (all_trips['distance_km'] < boundaries[i])]
+    all_trips_distance = all_trips[(all_trips['distance_estimated'] >= boundaries[i-1]) & (all_trips['distance_estimated'] < boundaries[i])]
     bluebikes_distance = bluebikes_trips_augmented[(bluebikes_trips_augmented['distance_estimated'] >= boundaries[i-1]) & (bluebikes_trips_augmented['distance_estimated'] < boundaries[i])]
     cycle_trips_count = all_trips_distance.shape[0]
     bluebikes_count = bluebikes_distance.shape[0]
@@ -88,3 +88,23 @@ print("Cycle trips total {}, bluebikes trips {}\n ratio {:.2f}, difference {}".f
 # # Print out the statistics
 # model.summary()
 # %%
+
+all_trips = get_data('./data/all_trips_bluebikes_replacements.csv')
+
+cycle_total = 0
+bluebikes_total = 0
+boundaries = [0, 1, 2.5, 5, 10, 10000]
+
+all_trips['distance_km'] = all_trips.apply(lambda x: x['distance_miles'] * 1.6, axis=1)
+bluebikes_trips_augmented = all_trips[all_trips['mode'] == 'SHARED_BIKE']
+all_trips = all_trips[all_trips['mode'] == 'BIKING']
+
+for i in range(1, len(boundaries)):
+    all_trips_distance = all_trips[(all_trips['distance_km'] >= boundaries[i-1]) & (all_trips['distance_km'] < boundaries[i])]
+    bluebikes_distance = bluebikes_trips_augmented[(bluebikes_trips_augmented['distance_km'] >= boundaries[i-1]) & (bluebikes_trips_augmented['distance_km'] < boundaries[i])]
+    bluebikes_count = bluebikes_distance.shape[0]
+    cycle_trips_count = all_trips_distance.shape[0] + bluebikes_count
+    cycle_total += cycle_trips_count
+    bluebikes_total += bluebikes_count
+    print("Distance {} to {}\n\t total {} bluebikes {}, ratio {:.2f}, difference {}".format(boundaries[i-1], boundaries[i], cycle_trips_count, bluebikes_count, bluebikes_count / cycle_trips_count, bluebikes_count - cycle_trips_count))
+print("Cycle trips total {}, bluebikes trips {}\n ratio {:.2f}, difference {}".format(cycle_total, bluebikes_total, bluebikes_total / cycle_total, bluebikes_total - cycle_total))
