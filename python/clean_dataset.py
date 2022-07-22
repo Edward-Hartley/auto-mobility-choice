278d2ea2e42bffc892f2b6088e31e8d4cf94e272
Edward-HartleyICL <edward.hartley20@imperial.ac.uk>
2022-07-22 16:35:47 +0100

    feat: people files cleaned too

--- python/clean_dataset.py
+++ python/clean_dataset.py
@@ -15,4 +15,17 @@ all_trips = all_trips[all_trips['mode'] != 'COMMERCIAL']
 
 # Store the cleaned dataset
 store_data(all_trips, './data/all_trips_filtered.csv')
+
 # %%
+
+workers = get_data(people_path_workers)
+residents = get_data(people_path_residents)
+
+
+
+all_people = combined_unique_rows(workers, residents, 'person_id')
+
+# remove columns that are not needed
+all_people.drop(['BLOCKGROUP_home', 'BLOCKGROUP_work', 'lat_home', 'lng_home', 'lat_work', 'lng_work', 'household_id', 'resident_type', 'race_ethnicity'], axis=1, inplace=True)
+
+store_data(all_people, "./data/all_people.csv")
